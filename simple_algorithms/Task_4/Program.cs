410b1197218b787f580416e5109a99d9f66c1b80
Lex <vasickin@mail.ru>
2024-02-21 08:31:52 +0300

    last commit

--- simple_algorithms/Task_4/Program.cs
+++ simple_algorithms/Task_4/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает натуральное число N,
+// а на выходе показывает его цифры через запятую.
+
+Console.Clear();
+
+Console.Write($"Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 0)
+{
+    Console.WriteLine($"{num}");
+}
+else
+{
+    Console.WriteLine("Не корректно введённое число");
+}
+
+string numberString = num.ToString();
+
+               for (int i = 0; i < numberString.Length; i++)
+        {
+            Console.Write(numberString[i]);
+
+            if (i != numberString.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+
+
+
